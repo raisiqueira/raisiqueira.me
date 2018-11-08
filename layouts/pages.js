89bac9bd5806e9fdc3892fdccf861209a9fd1944
Raí Siqueira <rai93siqueira@gmail.com>
2018-11-07 22:33:59 -0300

    add prefer-color-scheme to css media
    Signed-off-by: Raí Siqueira <rai93siqueira@gmail.com>

--- layouts/pages.js
+++ layouts/pages.js
@@ -1,38 +1,52 @@
-import Head from 'next/head';
-import pkg from '../package';
+import Head from "next/head";
+import pkg from "../package";
 
-export default ({children}) => (
-  <main style={{minHeight: '100vh'}}>
+export default ({ children }) => (
+  <main style={{ minHeight: "100vh" }}>
     <Head>
       <title>
-        {pkg.name} — {pkg.description}
+        {pkg.author} — {pkg.description}
       </title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <meta name="description" content={pkg.description} />
-      <meta name="keywords" content={pkg.keywords.join (', ')} />
+      <meta name="keywords" content={pkg.keywords.join(", ")} />
     </Head>
     {children}
     <style jsx global>
       {`
-      * {
-        padding: 0;
-        margin: 0;
-        -webkit-font-smoothing: antialiased;
-        box-sizing: border-box;
-        font-family: -apple-system, system-ui, BlinkMacSystemFont,
-          'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-      }
-      main {
-        background-color: #fff;
-      }
-      a {
-        text-decoration: none;
-      }
-      li {
-        list-style: none;
-      }
-    `}
+        * {
+          padding: 0;
+          margin: 0;
+          -webkit-font-smoothing: antialiased;
+          box-sizing: border-box;
+          font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI",
+            Roboto, "Helvetica Neue", Arial, sans-serif;
+        }
+        :root {
+          --bg-color: #fff;
+          --main-text-color: #000;
+          --secondary-text-color: #ababab;
+          --avatar-border: none;
+        }
+        @media (prefer-color-scheme: dark) {
+          :root {
+            --bg-color: #000;
+            --main-text-color: #fff;
+            --secondary-text-color: #d2d2d2;
+            --avatar-border: 1px solid #fff;
+          }
+        }
+        main {
+          background-color: var(--bg-color);
+        }
+        a {
+          text-decoration: none;
+        }
+        li {
+          list-style: none;
+        }
+      `}
     </style>
   </main>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import {Component} from 'react';
-import Typing from 'react-typing-animation';
-import Page from '../layouts/pages';
-import Avatar from '../components/Avatar';
+import { Component } from "react";
+import Typing from "react-typing-animation";
+import Page from "../layouts/pages";
+import Avatar from "../components/Avatar";
 
-const imgUri = 'https://avatars0.githubusercontent.com/u/2914170?s=460&v=4';
+const imgUri = "https://avatars0.githubusercontent.com/u/2914170?s=460&v=4";
 // Init of the layout
 class Home extends Component {
-  render () {
+  render() {
     return (
       <Page>
         <div>
@@ -22,26 +22,20 @@ class Home extends Component {
               <li>
                 <a href="https://twitter.com/rai_siqueira" target="_blank">
                   Twitter
-                </a>
-                {' '}
-                -
-                {' '}
+                </a>{" "}
+                -{" "}
               </li>
               <li>
                 <a href="https://github.com/raisiqueira" target="_blank">
                   Github
-                </a>
-                {' '}
-                -
-                {' '}
+                </a>{" "}
+                -{" "}
               </li>
               <li>
                 <a href="https://linkedin.com/in/raisiqueira" target="_blank">
                   LinkedIn
-                </a>
-                {' '}
-                -
-                {' '}
+                </a>{" "}
+                -{" "}
               </li>
               <li>
                 <a href="https://instagram.com/rai_siqueira" target="_blank">
@@ -59,8 +53,11 @@ class Home extends Component {
                 justify-content: flex-end;
                 flex-direction: column;
               }
+              h1 {
+                color: var(--main-text-color);
+              }
               h2 {
-                color: #ababab;
+                color: var(--secondary-text-color);
                 line-height: 30px;
               }
               ul {
@@ -69,28 +66,28 @@ class Home extends Component {
               }
               li {
                 font-size: 13px;
-                color: #ababab;
+                color: var(--secondary-text-color);
                 padding-right: 5px;
               }
               a,
               span {
-                color: #ababab;
+                color: var(--secondary-text-color);
                 margin-right: 5px;
                 cursor: pointer;
               }
               a:hover,
               span:hover {
-                color: #000;
+                color: var(--main-text-color);
               }
               h3 {
                 font-weight: 400;
                 font-size: 14px;
-                color: #ababab;
+                color: var(--secondary-text-color);
                 margin-top: 16px;
               }
               h3 a {
                 font-weight: 700;
-                color: #000;
+                color: var(--main-text-color);
                 margin-right: 0;
               }
               h3 a:hover {
